2012dd79f915a1b322a1150ab962c3807215f496
Justin Kimchi <kjustin521@gmail.com>
2024-06-22 11:53:42 -0500

    Updates to build

--- cogs/builds.py
+++ cogs/builds.py
@@ -13,41 +13,45 @@ class Builds(commands.Cog):
 
   @commands.Cog.listener()
   async def on_raw_message_delete(self, event):
-    if event.channel_id == self.accepted_builds:
-      print('TBD')
+    if event.channel_id != self.accepted_builds:
+      return
+    
+    # Delete build from channel
+    db.execute(query.BUILD_DELETE_BY_ID, event.message_id)
 
   @commands.Cog.listener()
   async def on_raw_reaction_add(self, event):
     # Check if reaction is in pending builds channel
-    if event.channel_id == self.pending_builds:
-      id_list = db.fetch(query.PENDING_BUILD_MESSAGE_ID_QUERY)
+    if event.channel_id != self.pending_builds:
+      return
+    
+    messageExists = db.fetch(query.PENDING_BUILD_MESSAGE_ID_QUERY, event.message_id)
       
-      for id in id_list:
-        if int(id[0]) == event.message_id:
-          # Get info from database of build
-          data = db.fetch(query.PENDING_BUILD_QUERY, id[0])
-          print(data)
+    if len(messageExists) != 0:
+      # Get info from database of build
+      data = db.fetch(query.PENDING_BUILD_QUERY, event.message_id)
+      print(data) 
 
-          # Remove from pending builds
-          db.execute(query.PENDING_BUILD_DELETE_BY_ID, id[0])
+      # Remove from pending builds
+      db.execute(query.PENDING_BUILD_DELETE_BY_ID, event.message_id)
 
-          # Parse data
-          name = data[0][0]
+      # Parse data
+      name = data[0][0]
+      author = data[0][1]
 
-          pendingChannel = self.bot.get_channel(event.channel_id)
-          pendingMessage = await pendingChannel.fetch_message(event.message_id)
+      pendingChannel = self.bot.get_channel(event.channel_id)
+      pendingMessage = await pendingChannel.fetch_message(event.message_id)
 
-          # Send image into builds channel
-          buildsChannel = self.bot.get_channel(self.accepted_builds)
-          if pendingMessage.attachments:
-            message = await buildsChannel.send(file= await pendingMessage.attachments[0].to_file())
+      # Send image into builds channel
+      buildsChannel = self.bot.get_channel(self.accepted_builds)
+      if pendingMessage.attachments:
+        message = await buildsChannel.send(file= await pendingMessage.attachments[0].to_file())
 
-          # Add pending build to database
-          db.execute(query.BUILD_INSERT, name, message.id)
-          break
+      # Add pending build to database
+      db.execute(query.BUILD_INSERT, name, message.id, author)
 
   @app_commands.command(name="requestaddbuild", description="Request to add a build to the database by attaching an image.")
-  async def requestaddbuild(self, interaction: discord.Interaction, image: discord.Attachment, name: str) -> None:
+  async def requestaddbuild(self, interaction: discord.Interaction, image: discord.Attachment, name: str, author: str = "") -> None:
     # Check to make sure character name is valid
     alias = name.lower()
     charName = db.fetch(query.BUILD_NAME_QUERY, alias)
@@ -61,7 +65,7 @@ class Builds(commands.Cog):
     message = await channel.send(file= await image.to_file())
 
     # Add to pending builds table
-    db.execute(query.PENDING_BUILD_INSERT, charName[0][0], message.id)
+    db.execute(query.PENDING_BUILD_INSERT, charName[0][0], message.id, author)
 
     await interaction.response.send_message("Request sent!", ephemeral=True)
 
@@ -90,11 +94,12 @@ class Builds(commands.Cog):
     # Send the build in channel
     channel = self.bot.get_channel(self.accepted_builds)
     message = await channel.fetch_message(int(build[0][0]))
+    author = build[0][1]
 
     embed = discord.Embed (
       colour = discord.Colour.brand_green(),
-      description="Here's a random build from my database!",
-      title=foundName
+      description = "Here's a random build from my database!",
+      title = foundName if author == "" else foundName + f" by {author}"
     )
 
     if message.attachments:
